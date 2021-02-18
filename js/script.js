e1c183d4d14a50105d21414fc6e25c013dfe3747
JenyaKyrychenko <happyshi333@gmail.com>
2021-02-18 21:21:29 +0200

    todo: footer

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+   slidesPerView: 3,
+   centeredSlides: true,
+   spaceBetween: 50,
+   initialSlide:1,
+ });
+
+ (function(){
+    const header = document.querySelector('.header')
+    window.onscroll = () => {
+       if(window.pageYOffset > 100){
+          header.classList.add('js-header')
+       } else {
+          header.classList.remove('js-header')
+       }
+    }
+ }())
