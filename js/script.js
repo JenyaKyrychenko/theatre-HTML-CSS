0ed545471a2adff52d15908d8a0cca3823642640
JenyaKyrychenko <happyshi333@gmail.com>
2021-02-19 18:03:03 +0200

    mobile done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ var swiper = new Swiper('.swiper-container', {
    spaceBetween: 50,
    initialSlide: 1,
    breakpoints:{
+      1440:{
+         slidesPerView:3,
+      },
+      1023:{
+         slidesPerView:2,
+      },
       992:{
          slidesPerView:2,
       },
@@ -29,3 +35,26 @@ var swiper = new Swiper('.swiper-container', {
        }
     }
  }())
+
+ (function(){
+   const burgerItem = document.querySelector('.burger')
+   const menu = document.querySelector('.header__nav')
+   const menuCloseItem = document.querySelector('.header__nav-close')
+   const menuLinks = document.querySelectorAll('.header__link')
+
+   burgerItem.addEventListener('click', () => {
+       menu.classList.add('header__nav_active')
+   })
+
+   menuCloseItem.addEventListener('click', () =>{
+       menu.classList.remove('header__nav_active')
+   }) 
+
+   if(window.innerWidth < 768){
+       menuLinks.forEach(item =>{
+           item.addEventListener('click', ()=>{
+               menu.classList.remove('header__nav_active')
+           })
+       })
+   }
+}());
